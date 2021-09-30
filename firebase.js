f6652ef01c806b3bef098a0faaa8ab23b2c2069a
RKC3000 <ramakrishna.chhipa99@gmail.com>
2021-09-30 10:43:11 +0530

    Some changes

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBy_KWn_hriwy2Ev7MP-LdIa3c_g8f-SUY",
+    authDomain: "zenith-e21aa.firebaseapp.com",
+    projectId: "zenith-e21aa",
+    storageBucket: "zenith-e21aa.appspot.com",
+    messagingSenderId: "927355970295",
+    appId: "1:927355970295:web:63ca7ef7e63795163d29f2",
+    measurementId: "G-9RK0ZW8VZK"
+  };
