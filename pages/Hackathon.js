0d47ddb76e8c78b879609eb16c3400ed60d4a012
RKC3000 <ramakrishna.chhipa99@gmail.com>
2021-10-06 13:45:11 +0530

    Hackathon 90% fixed and speaker data comment out

--- pages/Hackathon.js
+++ pages/Hackathon.js
@@ -13,17 +13,19 @@ function Hackathon() {
       <Navbar />
       <div className="mt-32">
         {/* Heading */}
-        <h1 className="text-7xl font-extraboldbold text-center underline ml-56 mb-3 md:ml-0">
+        <h1 className="text-2xl font-extrabold text-center md:ml-56 mb-3 md:text-7xl">
           Hackathon
         </h1>
 
         {/* About + Illustration */}
-        <div className="flex ml-20">
+        <div className="flex text-center md:ml-20">
           {/* about */}
           <div className="flex-1 p-3">
             <div>
-              <h1 className="text-6xl font-semibold">About</h1>
-              <p className="w-[450px] font-semibold mt-7">
+              <h1 className="text-3xl font-bold underline md:text-6xl">
+                About
+              </h1>
+              <p className=" font-semibold mt-7 md:w-[450px]">
                 Bright minds with innovative ideas are the core to simplified
                 hacks that makes one&apos;s life easier. The journey from
                 naturally obtained light to the light from high temperature
@@ -37,7 +39,7 @@ function Hackathon() {
             </div>
           </div>
           {/* Illustration */}
-          <div className="flex-1 h-[600px] w-[600px] ">
+          <div className="hidden flex-1 h-[600px] w-[600px] ">
             <lottie-player
               id="firstLottie"
               ref={ref}
@@ -53,15 +55,17 @@ function Hackathon() {
 
         {/* Themes */}
         <div>
-          <h1 className="text-6xl text-center font-semibold underline mb-5 ">
+          <h1 className="text-3xl text-center font-bold underline md:text-6xl mb-5 ">
             Theme
           </h1>
 
-          <div className="grid grid-cols-2 items-center bg-red-500 lg:mt-2 lg:p-5">
-            {/* details */}
-            <div className="w-[380px] ml-20">
-              <h1 className="text-2xl font-bold ">Health Hack:- </h1>
-              <p className="text-xl font-medium">
+          <div className="grid grid-rows-1 gap-y-5 items-center text-center ml-3 md:ml-52 md:grid-cols-2">
+            {/* 1st theme */}
+            <div className="bg-red-500 w-[300px] h-[400px] p-1 md:w-[400px]">
+              <h1 className="text-lg font-bold p-2 mb-2 md:text-2xl ">
+                Hack the Health:-
+              </h1>
+              <p className="text-base font-medium md:text-xl">
                 Health being an important aspect of one&apos;s life is usually
                 neglected these days due to one&apos;s hectic life. A breach of
                 one&apos;s peace, ability and focus is wildly disturbed due to
@@ -71,38 +75,12 @@ function Hackathon() {
                 and well maintained physique.
               </p>
             </div>
-            {/* Illustration */}
-            <div className="w-96 h-96 ml-32">
-              <lottie-player
-                id="firstLottie"
-                ref={ref}
-                autoplay
-                loop
-                mode="normal"
-                background="transparent"
-                speed="2"
-                src="https://assets2.lottiefiles.com/packages/lf20_q0nn8kie.json"
-              ></lottie-player>
-            </div>
-          </div>
-          <div className="grid grid-cols-2 items-center bg-blue-500 lg:mt-2 lg:p-5">
-            {/* Illustration */}
-            <div className="w-96 h-96">
-              <lottie-player
-                id="firstLottie"
-                ref={ref}
-                autoplay
-                loop
-                mode="normal"
-                background="transparent"
-                speed="1"
-                src="https://assets10.lottiefiles.com/packages/lf20_bqmgf5tx.json"
-              ></lottie-player>
-            </div>
-            {/* details */}
-            <div className="w-[380px] ml-20">
-              <h1 className="text-2xl font-bold ">Cosmic Hack:- </h1>
-              <p className="text-xl font-medium">
+            {/* 2nd theme */}
+            <div className="bg-blue-500 w-[300px] h-[400px] p-1 md:w-[400px]">
+              <h1 className="text-lg font-bold p-2 mb-2 md:text-2xl">
+                Hack the Cosmos:-{" "}
+              </h1>
+              <p className="text-base font-medium md:text-xl">
                 How many of you guys know that there lyes a place named Kuiper
                 ?? Well for the ones who don&apos;t know its an enormous ring of
                 astroids named Kuiper belt. This is where you&apos;ll find dwarf
@@ -114,12 +92,12 @@ function Hackathon() {
                 people an imaginary tour of cosmos on their screens
               </p>
             </div>
-          </div>
-          <div className="grid grid-cols-2 items-center bg-green-500 lg:mt-2 lg:p-5">
-            {/* details */}
-            <div className="w-[450px] md:ml-20">
-              <h1 className="text-2xl font-bold ">Tech Hack:- </h1>
-              <p className="text-xl font-medium">
+            {/* 3rd theme */}
+            <div className="bg-green-500 w-[300px] h-[400px] p-1 md:w-[400px]">
+              <h1 className="text-lg font-bold p-2 mb-2 md:text-2xl">
+                Hack the Tech:-{" "}
+              </h1>
+              <p className="text-lg font-medium md:text-xl">
                 This four letter word is enough to create its own world of
                 virtual reality or even one&apos;s personalised assistant that
                 can cut their long journeys of manual work and spare them with
@@ -132,8 +110,96 @@ function Hackathon() {
                 can really conjugate.
               </p>
             </div>
-            {/* Illustration */}
-            <div className="w-96 h-96 md:ml-36">
+            {/* 4th theme */}
+            <div className="bg-yellow-500 w-[300px] h-[400px] p-1 md:w-[400px]">
+              <h1 className="text-lg font-bold p-2 mb-2 md:text-2xl">
+                Hack the Custom:-{" "}
+              </h1>
+              <p className="text-lg font-medium md:text-xl">
+                Pick up an problem of your own , show its worth with the
+                application that you develop and get ur zenith as an idea is
+                just a root with its branches trying to have their own way.
+              </p>
+            </div>
+          </div>
+
+          {/* <div className="flex items-center justify-between md:{grid grid-cols-2 items-center} bg-red-500 md:mt-2 lg:p-5"> */}
+          {/* details */}
+          {/* <div className="w-[380px] ml-20">
+              <h1 className="text-2xl font-bold ">Hack the Health:- </h1>
+              <p className="text-xl font-medium">
+                Health being an important aspect of one&apos;s life is usually
+                neglected these days due to one&apos;s hectic life. A breach of
+                one&apos;s peace, ability and focus is wildly disturbed due to
+                any health or medical ambiguity. Awareness with easy
+                implementation and wide focus with making it health look fun may
+                make people keener towars the goal of achieving an healthy mind
+                and well maintained physique.
+              </p>
+            </div> */}
+          {/* Illustration */}
+          {/* <div className="w-96 h-96 ml-32">
+              <lottie-player
+                id="firstLottie"
+                ref={ref}
+                autoplay
+                loop
+                mode="normal"
+                background="transparent"
+                speed="2"
+                src="https://assets2.lottiefiles.com/packages/lf20_q0nn8kie.json"
+              ></lottie-player>
+            </div>
+          </div> */}
+          {/* <div className="flex items-center justify-between md:{grid grid-cols-2 items-center} bg-blue-500 mt-2 lg:p-5"> */}
+          {/* Illustration */}
+          {/* <div className="w-96 h-96">
+              <lottie-player
+                id="firstLottie"
+                ref={ref}
+                autoplay
+                loop
+                mode="normal"
+                background="transparent"
+                speed="1"
+                src="https://assets10.lottiefiles.com/packages/lf20_bqmgf5tx.json"
+              ></lottie-player>
+            </div> */}
+          {/* details */}
+          {/* <div className="w-[380px] ml-20">
+              <h1 className="text-2xl font-bold ">Hack the Cosmos:- </h1>
+              <p className="text-xl font-medium">
+                How many of you guys know that there lyes a place named Kuiper
+                ?? Well for the ones who don&apos;t know its an enormous ring of
+                astroids named Kuiper belt. This is where you&apos;ll find dwarf
+                planet Pluto. Well thats the case now its you that know it
+                existed but can we spread it ahead ? Well new innovations in
+                tech and exploration of cosmos have their own fantastic
+                storyline which makes them both interesting and fascinating at
+                the same time. Now take it ahead and make a difference by giving
+                people an imaginary tour of cosmos on their screens
+              </p>
+            </div>
+          </div> */}
+          {/* <div className="flex items-center justify-between md:{grid grid-cols-2 items-center} bg-green-500 mt-2 lg:p-5"> */}
+          {/* details */}
+          {/* <div className="w-[450px] md:ml-20">
+              <h1 className="text-2xl font-bold ">Hack the Tech:- </h1>
+              <p className="text-xl font-medium">
+                This four letter word is enough to create its own world of
+                virtual reality or even one&apos;s personalised assistant that
+                can cut their long journeys of manual work and spare them with
+                even more time to focus on things with greater importance.
+                Trending technology can grand us an better version on its past
+                and now when we explore it and dig deeper we may find even new
+                wonders. Give us an technology that u think is wonderful and
+                helps us getting even more closer to perfection. Give us
+                it&apos;s gist or how may ur application show what is it that it
+                can really conjugate.
+              </p>
+            </div> */}
+          {/* Illustration */}
+          {/* <div className="w-96 h-96 md:ml-36">
               <lottie-player
                 id="firstLottie"
                 ref={ref}
@@ -145,10 +211,10 @@ function Hackathon() {
                 src="https://assets8.lottiefiles.com/packages/lf20_m9zragkd.json"
               ></lottie-player>
             </div>
-          </div>
-          <div className="grid grid-cols-2 items-center bg-yellow-500 lg:mt-2 lg:p-5">
-            {/* Illustration */}
-            <div className="w-80 md:w-96 md:h-96">
+          </div> */}
+          {/* <div className="flex items-center justify-between md:{grid grid-cols-2 items-center} bg-yellow-500 lg:mt-2 lg:p-5"> */}
+          {/* Illustration */}
+          {/* <div className="w-96 h-96">
               <lottie-player
                 id="firstLottie"
                 ref={ref}
@@ -159,17 +225,17 @@ function Hackathon() {
                 speed="2"
                 src="https://assets8.lottiefiles.com/packages/lf20_z3wd7moi.json"
               ></lottie-player>
-            </div>
-            {/* details */}
-            <div className="w-[450px] ml-20">
-              <h1 className="text-2xl font-bold ">Custom Hacks:- </h1>
+            </div> */}
+          {/* details */}
+          {/* <div className="w-[450px] ml-20">
+              <h1 className="text-2xl font-bold ">Hack the Custom:- </h1>
               <p className="text-xl font-medium">
                 Pick up an problem of your own , show its worth with the
                 application that you develop and get ur zenith as an idea is
                 just a root with its branches trying to have their own way.
               </p>
-            </div>
-          </div>
+            </div> */}
+          {/* </div> */}
         </div>
       </div>
       <Footer />
